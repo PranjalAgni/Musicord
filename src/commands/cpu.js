9beaf9b36f562d02017221dce598b32f1697e97a
beingPranjal <pranjal.agnihotri6@gmail.com>
2024-04-19 23:58:20 +0530

    :memo: updated message

--- src/commands/cpu.js
+++ src/commands/cpu.js
@@ -3,7 +3,7 @@ const os = require('os');
 const handler = (message) => {
   const cpuList = os.cpus();
   const defaultCpu = cpuList.at(0);
-  message.channel.send(`Model is: ${defaultCpu.model}`);
+  message.channel.send(`CPU model is: ${defaultCpu.model}`);
 };
 
 module.exports = {
